7ca21a5071acefc957ac13da6dd26f291e9e0bb0
Amanda Kalk <amanda.kalk@thinkbrownstone.com>
2017-04-11 19:59:55 -0400

    Adding our first controller and attaching a single dog.

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 (function() {
 	var app = angular.module('dogApp', []);
+
+	app.controller('DogAppController', function() {
+		this.dog = {
+			"name": "Kyle",
+			"img": "//pbs.twimg.com/media/C8576jrW0AEYWFy.jpg",
+			"description": "This is Kyle. He made a joke about your shoes, then stuck his tongue out at you. Uncalled for. Step the h*ck up Kyle. 11/10 would forgive",
+			"weRating": 11,
+			"owner": {
+				"name": "#iamMEHAR",
+				"twitter": "sardar_kutto_ka"
+			}
+		};
+	});
 })();
